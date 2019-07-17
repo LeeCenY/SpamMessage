eb037f5b47e1586cb06e70d005c5b6a211ded8bf
dinghing <dinghing154@gmail.com>
2019-07-17 14:30:37 +0900

    complete the whole project

---
+++ MessageFilterExtension/MessageFilterExtension.swift
@@ -0,0 +1,114 @@
+//
+//  MessageFilterExtension.swift
+//  MessageFilterExtension
+//
+//  Created by ding qi on 2019/07/17.
+//  Copyright © 2019 ding qi. All rights reserved.
+//
+
+import IdentityLookup
+import CoreML
+
+final class MessageFilterExtension: ILMessageFilterExtension {}
+
+extension MessageFilterExtension: ILMessageFilterQueryHandling {
+    
+    func handle(_ queryRequest: ILMessageFilterQueryRequest, context: ILMessageFilterExtensionContext, completion: @escaping (ILMessageFilterQueryResponse) -> Void) {
+        // First, check whether to filter using offline data (if possible).
+        let offlineAction = self.offlineAction(for: queryRequest)
+        
+        switch offlineAction {
+        case .allow, .filter:
+            // Based on offline data, we know this message should either be Allowed or Filtered. Send response immediately.
+            let response = ILMessageFilterQueryResponse()
+            response.action = offlineAction
+            
+            completion(response)
+            
+        case .none:
+            // Based on offline data, we do not know whether this message should be Allowed or Filtered. Defer to network.
+            // Note: Deferring requests to network requires the extension target's Info.plist to contain a key with a URL to use. See documentation for details.
+            context.deferQueryRequestToNetwork() { (networkResponse, error) in
+                let response = ILMessageFilterQueryResponse()
+                response.action = .none
+                
+                if let networkResponse = networkResponse {
+                    // If we received a network response, parse it to determine an action to return in our response.
+                    response.action = self.action(for: networkResponse)
+                } else {
+                    NSLog("Error deferring query request to network: \(String(describing: error))")
+                }
+                
+                completion(response)
+            }
+            
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    private func offlineAction(for queryRequest: ILMessageFilterQueryRequest) -> ILMessageFilterAction {
+        // Replace with logic to perform offline check whether to filter first (if possible).
+        var text:String
+        guard let messageBody = queryRequest.messageBody?.lowercased() else { return .none }
+        let vec = tfidf(sms: messageBody)
+        do {
+            let predict = try MessageClassifier().prediction(message: vec).label
+            text = predict
+        }catch{
+            text = "No Prediction"
+        }
+        
+        if text == "spam"{
+            return .filter
+        }
+        
+        return .allow
+    }
+    
+    private func action(for networkResponse: ILNetworkResponse) -> ILMessageFilterAction {
+        // Replace with logic to parse the HTTP response and data payload of `networkResponse` to return an action.
+        return .none
+    }
+    
+    func tfidf(sms: String) -> MLMultiArray{
+        let wordsFile = Bundle.main.path(forResource: "words_ordered", ofType: "txt")
+        let smsFile = Bundle.main.path(forResource: "SMSSpamCollection", ofType: "txt")
+        do {
+            let wordsFileText = try String(contentsOfFile: wordsFile!, encoding: String.Encoding.utf8)
+            var wordsData = wordsFileText.components(separatedBy: .newlines)
+            wordsData.removeLast() // Trailing newline.
+            let smsFileText = try String(contentsOfFile: smsFile!, encoding: String.Encoding.utf8)
+            var smsData = smsFileText.components(separatedBy: .newlines)
+            smsData.removeLast() // Trailing newline.
+            let wordsInMessage = sms.split(separator: " ")
+            let vectorized = try MLMultiArray(shape: [NSNumber(integerLiteral: wordsData.count)], dataType: MLMultiArrayDataType.double)
+            for i in 0..<wordsData.count{
+                let word = wordsData[i]
+                if sms.contains(word){
+                    var wordCount = 0
+                    for substr in wordsInMessage{
+                        if substr.elementsEqual(word){
+                            wordCount += 1
+                        }
+                    }
+                    let tf = Double(wordCount) / Double(wordsInMessage.count)
+                    var docCount = 0
+                    for sms in smsData{
+                        if sms.contains(word) {
+                            docCount += 1
+                        }
+                    }
+                    let idf = log(Double(smsData.count) / Double(docCount))
+                    vectorized[i] = NSNumber(value: tf * idf)
+                } else {
+                    vectorized[i] = 0.0
+                }
+            }
+            return vectorized
+        } catch {
+            return MLMultiArray()
+        }
+    }
+
+}
